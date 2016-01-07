3830c8d9fedcb26f7db04917c56e54f53cf034df
Chandan Kumar <chandan.bhardwaj22@gmail.com>
2016-01-07 12:29:38 +0000

    updated hook for feature

--- src/test/java/com/test/bdd/steps/Hooks.java
+++ src/test/java/com/test/bdd/steps/Hooks.java
@@ -3,6 +3,7 @@ package com.test.bdd.steps;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
+import gherkin.formatter.model.Feature;
 
 /**
  * Created by cku04 on 08/12/2015.
@@ -10,7 +11,7 @@ import cucumber.api.java.Before;
 public class Hooks {
 
     @Before
-    public void before(Scenario s){
+    public void before(Feature s) {
         System.out.println("Before hook kicked off for scenario " + s.getName() + "***" + s.getClass());
     }
 
